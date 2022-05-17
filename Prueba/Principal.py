917621a386be4fdd8df9a5ff393d5ddd8a616c85
Julio Dominguez <chiapasjeremai@gmail.com>
2022-05-16 22:42:34 -0500

    Calculadora basica creando su funcion suma, resta, multiplicacion, division

---
+++ Prueba/Principal.py
@@ -0,0 +1,8 @@
+import CalculadoraBasica.calculadora as calcu
+objeto = calcu.cal(4,5)
+print("Calculadora operaciones basicas ")
+print("valores",objeto.numero1,objeto.numero2)
+print("Suma",objeto.sumar())
+print("Resta",objeto.restar())
+print("Multiplicacion",objeto.multiplicar())
+print("Division",objeto.dividir())
