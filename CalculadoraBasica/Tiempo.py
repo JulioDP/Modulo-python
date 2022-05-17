5f5a363e30530036b74c7a206308516bcae9a97a
Julio Dominguez <chiapasjeremai@gmail.com>
2022-05-17 12:32:12 -0500

    Agregando el modul calcular Tiempo de trabajo de un trabajador

---
+++ CalculadoraBasica/Tiempo.py
@@ -0,0 +1,24 @@
+import time
+from turtle import home
+
+
+class TiempoTrabajo:
+
+    def __init__(self,horaEntrada,minutoEntrada):
+        self.entradaHora = horaEntrada
+        self.entradaMinuto = minutoEntrada
+        self.hora = time.strftime('%H')
+        self.minuto = time.strftime('%M')
+
+
+    def verificarSalida(self):
+        veryHora = int(self.hora) - self.entradaHora
+        verMinuto = int(self.minuto) - self.entradaMinuto
+        if (veryHora > 7):
+            print("Hora completada favor de ir a descansar")
+        else:
+            print("Faltan",7 - veryHora ,"hora", verMinuto,"minuto para completar")
+
+ob = TiempoTrabajo(9,30)
+ob.verificarSalida()
+
